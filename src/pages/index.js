62b1bed09dc8af352a0f6a8970dd4a02a180903c
Mehdi Djabri <mehdi@Mehdis-MacBook-Pro.local>
2018-08-02 17:38:40 -0700

    testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Learn to design and code React apps</h1>
-    <p>Complete courses about the best tools and yo.</p>
+    <p>Complete courses about the best tools and testing.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
